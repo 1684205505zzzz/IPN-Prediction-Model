d7bd30fabf36990ef559c437975f51ae59082285
1684205505zzzz <buminchun0808@163.com>
2025-10-26 00:13:21 +0800

    Update 模型部署_新布局.py

--- 模型部署_新布局.py
+++ 模型部署_新布局.py
@@ -27,7 +27,7 @@ st.markdown("""
     
     /* 调整输入框宽度 */
     .stNumberInput > div > div > input {
-        max-width: 200px;
+        max-width: 100px;
     }
     
     /* 调整标签和输入框容器的宽度 */
@@ -375,6 +375,7 @@ with right_col:
         # 当还没有点击预测按钮时显示提示信息
 
         st.info("👈 Please enter the patient data in the left panel and click 'Predict' to see the results here.")
+
 
 
 
